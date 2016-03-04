760257f18dfb5c7977921bc609d7ae83415046a5
Xiong Hui <xionghuisquall@126.com>
2016-03-04 12:44:27 +0800

    update

--- naive_bayes/bayes.py
+++ naive_bayes/bayes.py
@@ -29,10 +29,10 @@ def trainNB0(trainMatrix, trainCategory):
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
     pAbusive = sum(trainCategory) / float(numTrainDocs)
-    p0Num = zeros(numWords);
-    p1Num = zeros(numWords);
-    p0Denom = 0.0;
-    p1Denom = 0.0;
+    p0Num = ones(numWords);
+    p1Num = ones(numWords);
+    p0Denom = 2.0;
+    p1Denom = 2.0;
     for i in range(numTrainDocs):
         if trainCategory[i] == 1:
             p1Num += trainMatrix[i]
@@ -40,6 +40,14 @@ def trainNB0(trainMatrix, trainCategory):
         else:
             p0Num += trainMatrix[i]
             p0Denom += sum(trainMatrix[i])
-    p1Vect = p1Num / p1Denom
-    p0Vect = p0Num / p0Denom
+    p1Vect = log(p1Num / p1Denom)
+    p0Vect = log(p0Num / p0Denom)
     return p0Vect, p1Vect, pAbusive
+
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    p1 = sum(vec2Classify * p1Vec) + log(pClass1)
+    p0 = sum(vec2Classify * p0Vec) + log(1 - pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
