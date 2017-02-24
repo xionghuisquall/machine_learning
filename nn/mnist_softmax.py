cdd03c3009b365f851c683f259084f4265e82c74
xionghuisquall <xionghuisquall@126.com>
2017-02-25 06:37:50 +0800

    add nn.mnist_softmax.py

---
+++ nn/mnist_softmax.py
@@ -0,0 +1,130 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+import argparse
+import gzip
+import os
+import sys
+import time
+
+import numpy
+from six.moves import urllib
+from six.moves import xrange  # pylint: disable=redefined-builtin
+import tensorflow as tf
+import  tensorflow.examples.tutorials.mnist.input_data
+
+SOURCE_URL = 'http://yann.lecun.com/exdb/mnist/'
+WORK_DIRECTORY = 'data'
+IMAGE_SIZE = 28
+NUM_CHANNELS = 1
+PIXEL_DEPTH = 255
+NUM_LABELS = 10
+VALIDATION_SIZE = 5000  # Size of the validation set.
+SEED = 66478  # Set to None for random seed.
+BATCH_SIZE = 64
+NUM_EPOCHS = 10
+EVAL_BATCH_SIZE = 64
+EVAL_FREQUENCY = 100  # Number of steps between evaluations.
+
+
+FLAGS = None
+
+def maybe_download(filename):
+  """Download the data from Yann's website, unless it's already here."""
+  if not tf.gfile.Exists(WORK_DIRECTORY):
+    tf.gfile.MakeDirs(WORK_DIRECTORY)
+  filepath = os.path.join(WORK_DIRECTORY, filename)
+  if not tf.gfile.Exists(filepath):
+    filepath, _ = urllib.request.urlretrieve(SOURCE_URL + filename, filepath)
+    with tf.gfile.GFile(filepath) as f:
+      size = f.size()
+    print('Successfully downloaded', filename, size, 'bytes.')
+  return filepath
+
+
+def extract_data(filename, num_images):
+  """Extract the images into a 4D tensor [image index, y, x, channels].
+
+  Values are rescaled from [0, 255] down to [-0.5, 0.5].
+  """
+  print('Extracting', filename)
+  with gzip.open(filename) as bytestream:
+    bytestream.read(16)
+    buf = bytestream.read(IMAGE_SIZE * IMAGE_SIZE * num_images * NUM_CHANNELS)
+    data = numpy.frombuffer(buf, dtype=numpy.uint8).astype(numpy.float32)
+    data = (data - (PIXEL_DEPTH / 2.0)) / PIXEL_DEPTH
+    # data = data.reshape(num_images, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS)
+    data = data.reshape(num_images, 784)
+    return data
+
+
+def extract_labels(filename, num_images):
+  """Extract the labels into a vector of int64 label IDs."""
+  print('Extracting', filename)
+  with gzip.open(filename) as bytestream:
+    bytestream.read(8)
+    buf = bytestream.read(1 * num_images)
+    labels = numpy.frombuffer(buf, dtype=numpy.uint8).astype(numpy.int64)
+    # labels = labels.reshape(num_images, 10)
+  return dense_to_one_hot(labels, 10)
+
+def dense_to_one_hot(labels_dense, num_classes):
+  """Convert class labels from scalars to one-hot vectors."""
+  num_labels = labels_dense.shape[0]
+  index_offset = numpy.arange(num_labels) * num_classes
+  labels_one_hot = numpy.zeros((num_labels, num_classes))
+  labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
+  return labels_one_hot.astype(numpy.float32)
+
+# Get the data.
+train_data_filename = maybe_download('train-images-idx3-ubyte.gz')
+train_labels_filename = maybe_download('train-labels-idx1-ubyte.gz')
+test_data_filename = maybe_download('t10k-images-idx3-ubyte.gz')
+test_labels_filename = maybe_download('t10k-labels-idx1-ubyte.gz')
+
+train_data = extract_data(train_data_filename, 60000)
+train_labels = extract_labels(train_labels_filename, 60000)
+test_data = extract_data(test_data_filename, 10000)
+test_labels = extract_labels(test_labels_filename, 10000)
+
+print(train_data.shape[0])
+print(train_data.shape[1])
+
+print(train_labels.shape[0])
+print(train_labels.shape[1])
+
+print(test_data.shape[0])
+
+# Generate a validation set.
+validation_data = train_data[:VALIDATION_SIZE, ...]
+validation_labels = train_labels[:VALIDATION_SIZE]
+train_data = train_data[VALIDATION_SIZE:, ...]
+train_labels = train_labels[VALIDATION_SIZE:]
+
+x = tf.placeholder("float", [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+y_ = tf.placeholder("float", [None, 10])
+cross_entropy = - tf.reduce_sum(y_ * tf.log(y))
+
+step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+init = tf.global_variables_initializer()
+
+sess = tf.Session()
+sess.run(init)
+
+size = train_data.shape[0]
+for begin in range(0, size, 60):
+    end = begin + 60
+    sess.run(step, feed_dict={x:train_data[begin:end, ...], y_:train_labels[begin:end, ...]})
+
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
+
+    error_rate = sess.run(accuracy, feed_dict={x: test_data, y_:test_labels})
+    print(error_rate)
